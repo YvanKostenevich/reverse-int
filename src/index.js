e0d1e3d695e2954f69f08a76b0e4f8fb3e7b78de
YvanKostenevich <YvanKostenevich@gmail.com>
2020-03-19 20:51:33 +0300

    feat: complete reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+
+    if (n < 0) {
+        let positiveN = n * -1;
+        return Number(positiveN.toString().split('').reverse().join('').toString());
+    } else {
+        return Number(n.toString().split('').reverse().join('').toString());
+    }
+};
